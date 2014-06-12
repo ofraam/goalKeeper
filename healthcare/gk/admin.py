a23cf858b66582e25dc8349dd723f39ddec8f8c6
Nithin Tumma <ntumma@college.harvard.edu>
2014-06-11 23:51:40 -0400

    added gitignore and created admin interface

--- healthcare/gk/admin.py
+++ healthcare/gk/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from gk.models import *
 
 # Register your models here.
+admin.site.register(Caregiver)
+admin.site.register(Patient)
+admin.site.register(Goal)
+admin.site.register(StatusUpdateType)
+admin.site.register(StatusUpdate)
+admin.site.register(FamilyMember)
+admin.site.register(Action)
