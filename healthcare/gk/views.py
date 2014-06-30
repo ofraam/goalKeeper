05a28ad6099eacff1674a01f3f9e21b6ade3e825
Logan Martin <logoman04@gmail.com>
2014-06-30 16:31:05 -0400

    6/30 Updates
    Add Goal form now sends info to the db. Make sure when you type a goal
    name it has no spaces in it.

--- healthcare/gk/views.py
+++ healthcare/gk/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from gk.models import *
+from django import forms
 
 # Create your views here.
 
 
 def home(request):
+	if (request.method == 'POST'):
+		form = AddGoalForm(request.POST)
+		if form.is_valid():
+			name = form.cleaned_data['goal_Name']
+			type = form.cleaned_data['Type']
+			notes = form.cleaned_data['Description']
+			active = True
+			patient = get_object_or_404(Patient, name="Billy Smith")
+			caregiverName = form.cleaned_data['caregiver']
+			caregiver = get_object_or_404(Caregiver, name = caregiverName)
+			newGoal = Goal.objects.create(name = name, notes = notes, active = active, caregiver = caregiver, patient = patient)
+			return HttpResponseRedirect('')
+	else:
+		form = AddGoalForm()
 	latest_goals = Goal.objects.order_by('name')[:5]
 	actions = Action.objects.all()
 	context = {'latest_goals' : latest_goals,
-				'actions' : actions}
+				'actions' : actions,
+				'AddGoalForm' : AddGoalForm,
+				}
 	return render(request, 'gk/Home.html', context)
 
 def goal(request, goal_name):
@@ -52,6 +69,34 @@ def profile(request):
 			   'updates' : updates,
 			   }
 	return render(request, 'gk/Profile.html', context)
+
+
+
+
+
+
+
+
+
+class AddGoalForm(forms.Form):
+	type_choices = [('0', u'Qualitative'), 
+					('1', u'Quantitative'),
+					]
+	caregiver_choices = []
+	val=0
+	for c in Caregiver.objects.all():
+		caregiver_choices.append((c.name, c))
+		val+=1
+	goal_Name = forms.CharField(max_length = 30)
+	Type = forms.ChoiceField(type_choices)
+	Description = forms.CharField()
+	caregiver = forms.ChoiceField(caregiver_choices)
+
+
+
+
+
+
 
 
 
