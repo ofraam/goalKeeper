ddfd4f8578d1012da85ee339e32299eae35078b9
Sebastian Gehrmann <s.gehrmann@outlook.com>
2016-10-04 22:02:57 -0400

    first urls.py fixed

--- healthcare/healthcare/urls.py
+++ healthcare/healthcare/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from healthcare import views as hc_views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,10 +8,10 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'healthcare.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$', 'healthcare.views.show_login', name='home'),
-   	url(r'^home/', 'healthcare.views.home', name='home'),  
+	url(r'^$', hc_views.show_login, name='home'),
+   	url(r'^home/', hc_views.home, name='home'),
     #check our own login and logout handlers too
-    url(r'^login/', 'healthcare.views.login_handler', name='login'),
-    url(r'^logout/', 'healthcare.views.logout_handler', name='logout'), 
-    url(r'^logs/', 'healthcare.views.logs'), 
+    url(r'^login/', hc_views.login_handler, name='login'),
+    url(r'^logout/', hc_views.logout_handler, name='logout'),
+    url(r'^logs/', hc_views.logs),
 ]
