e045897d0f1e2a3028c87c8df5a81f0e4f1eb48b
Sebastian Gehrmann <s.gehrmann@outlook.com>
2016-10-12 13:29:17 -0400

    bla

--- healthcare/gk/urls.py
+++ healthcare/gk/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import url, include
 from gk import views
 
 urlpatterns = [
-	url(r'^$', views.landing_page, name='home'),	
+	url(r'^$', views.landing_page, name='home'),
     url(r'^goal/(?P<goal_id>[\w ]+)/$', views.goal, name='goal'),
     url(r'^goal/(?P<goal_id>[\w ]+)/edit_status/$', views.edit_status, name='goal_edit'),
     url(r'^action/(?P<patient_id>[\w ]+)/$', views.action, name='action'),
     url(r'^contacts/(?P<patient_id>[\w ]+)/$', views.contacts, name='contacts'),
     url(r'^profile/(?P<patient_id>[\w ]+)/$', views.profile, name='profile'),
-    url(r'^(?P<user_id>\w+)/$', views.home, name='home'),    
+    url(r'^(?P<user_id>\w+)/$', views.home, name='home'),
 ]
 
 #urlpatterns += staticfiles_urlpatterns()
